8623f3559ffe265a62efb4ef384a425ea4018dac
Souvik <souvikroy1999ab@gmail.com>
2024-05-13 23:49:46 +0530

    initial commit

---
+++ Monolith/src/test/java/com/monolith/Monolith/MonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monolith.Monolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
